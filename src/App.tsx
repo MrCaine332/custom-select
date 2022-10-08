55d35f90f697c95a3bc9c429e5161a248bf6fbc4
Egor <ktp332@yandex.ru>
2022-10-08 15:21:20 +0300

    Finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 import Select from "./components/select/Select";
 
+
+const options = [
+  { name: "Firstdsfsdfsdfsdfsdfsdfsdfsddfsdfdfsageghfghgwdf", _id: 1},
+  { name: "Second", _id: 2},
+  { name: "Third", _id: 3},
+  { name: "Fourth", _id: 4},
+  { name: "Fifth", _id: 5}
+]
+
 function App() {
+
+  const [value1, setValue1] = useState<{ name: string, _id: number } | undefined>(undefined)
+  const [value2, setValue2] = useState<{ name: string, _id: number }[]>([])
+
   return (
     <div className="App">
       <div className="container">
-        <Select />
-        <Select />
+        <Select multiple={true} options={options} selected={value2} onChange={setValue2} />
+        <Select multiple={false} options={options} selected={value1} onChange={setValue1} />
       </div>
     </div>
   );
--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,20 +1,154 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import "./Select.scss"
 
+interface ISingleSelectProps {
+  multiple?: false,
+  selected?: { name: string, _id: number }
+  onChange: (item: { name: string, _id: number } | undefined) => void
+}
 
-const Select = () => {
+interface IMultiSelectProps {
+  multiple: true
+  selected: { name: string, _id: number }[]
+  onChange: (items: { name: string, _id: number }[]) => void
+}
+
+type ISelect = {
+  options: any[],
+} & (ISingleSelectProps | IMultiSelectProps)
+
+
+
+const Select: React.FC<ISelect> = ({
+  multiple,
+  options,
+  selected,
+  onChange
+}) => {
+  const [opened, setOpened] = useState<boolean>(false)
+  const [highlightedIndex, setHighlightedIndex] = useState(0)
+
+  const wrapRef = useRef<HTMLDivElement>(null)
+  const listRef = useRef<HTMLDivElement>(null)
+
+  const open = () => {
+    setOpened(prevState => !prevState)
+  }
+
+  const clear = (e: any) => {
+    e.stopPropagation()
+    multiple
+      ? onChange([])
+      : onChange(undefined)
+  }
+
+  const setValue = (option: { name: string, _id: number }) => {
+    multiple
+      ? selected.includes(option)
+        ? onChange(selected.filter((o) => o !== option ))
+        : onChange([...selected, option])
+      : onChange(option)
+  }
+
+  const isOptionSelected = (option: { name: string, _id: number }) => {
+    return multiple ? selected.includes(option) : selected?._id === option._id
+  }
+
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (!wrapRef.current?.contains(e.target as Node))
+        setOpened(false)
+    }
+    document.addEventListener('click', listener)
+    return () => document.removeEventListener('click', listener)
+  }, [])
+
+  useEffect(() => {
+    setHighlightedIndex(0)
+  }, [opened])
+
+  useEffect(() => {
+    const keyPressHandler = (e: KeyboardEvent) => {
+      if (e.target !== wrapRef.current) return
+      switch (e.code) {
+        case "Enter":
+          if (!opened){
+            setOpened(prevState => !prevState)
+            break
+          }
+          setValue(options[highlightedIndex])
+          setOpened(prevState => !prevState)
+          break
+        case "Space":
+          setOpened(prevState => !prevState)
+          break
+        case "ArrowUp":
+          if (highlightedIndex === 0)
+            break
+          setHighlightedIndex(prevState => prevState - 1)
+          break
+        case "ArrowDown":
+          if (highlightedIndex === options.length - 1)
+            break
+          setHighlightedIndex(prevState => prevState + 1)
+          break
+        case "Escape":
+        case "Tab":
+          if (opened) {
+            setOpened(prevState => !prevState)
+          }
+          break
+      }
+    }
+
+    wrapRef.current?.addEventListener('keydown', keyPressHandler)
+    return () => wrapRef.current?.removeEventListener('keydown', keyPressHandler)
+  }, [opened, highlightedIndex])
+
+
   return (
-    <div className="select_wrap">
-      <div>
-        afsdf
-      </div>
-      <div className="select_utils">
-        <div>&times;</div>
-        <hr className="line" />
-        <div>
-
+    <div className="select_wrap"
+         onClick={open}
+         tabIndex={0}
+         ref={wrapRef}
+    >
+      { multiple ?
+        <div className="selected_items">
+          { selected?.map((option) => (
+            <div key={ option._id } className="selected_item">
+              <div className="test">{ option.name }</div>
+              <div className="clear_btn" onClick={() => setValue(option)}>&times;</div>
+            </div>
+          )) }
+        </div> :
+        <div className="selected_item__single">
+          { selected?.name }
         </div>
+      }
+      <div className="select_utils">
+        <button className="clear_btn" onClick={clear}>&times;</button>
+        <hr className="line" />
+        <div className="caret" />
       </div>
+      { opened &&
+      <div className="select_list__wrap" ref={listRef}>
+        <ul className="select_list">
+          { options.map((option, index) => (
+            <li key={option._id}
+                onClick={() => setValue(option)}
+                onMouseEnter={() => setHighlightedIndex(index)}
+                className={
+                  `list_item 
+                  ${isOptionSelected(option) && 'list_item__selected'} 
+                  ${highlightedIndex === index && 'list_item__highlighted'}`
+                }
+            >
+              {option.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+      }
     </div>
   );
 };
