4d94cb815f52c0ac079eb2babd63e9332ca92bff
Egor <ktp332@yandex.ru>
2022-10-08 12:45:12 +0300

    select

---
+++ src/components/select/Select.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Select.scss"
+
+
+const Select = () => {
+  return (
+    <div className="select_wrap">
+      <div>
+        afsdf
+      </div>
+      <div className="select_utils">
+        <div>&times;</div>
+        <hr className="line" />
+        <div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Select;
